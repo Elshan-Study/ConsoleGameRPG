8b66cc1966890d2568c149dc53cd6ef65d84a021
Elshan-Study <isaye_ee51@itstep.edu.az>
2025-04-11 10:53:32 +0400

    Archetype class get and set methods delete

--- Archetype.h
+++ Archetype.h
@@ -2,15 +2,16 @@
 class Archetype 
 {
 protected:
+	size_t HP;
+	size_t AP;
+public:
 	size_t Brawn;
 	size_t Agility;
 	size_t Intellect;
 	size_t Cunning;
 	size_t Willpower;
 	size_t Presence;
-	size_t HP;
-	size_t AP;
-public:
+
 	Archetype() : Brawn(1), Agility(1), Intellect(1), Cunning(1), Willpower(1), Presence(1), HP(1), AP(1) {};
 	explicit Archetype(size_t brawn, size_t agility, size_t intellect, size_t cunning, size_t willpower, size_t presence) : HP(0), AP(0)
 	{
@@ -24,21 +25,6 @@ public:
 	virtual ~Archetype() = default;
 	virtual void setHP() = 0;
 	virtual void setAP() = 0;
-
-	size_t getBrawn() const { return Brawn; }
-	size_t getAgility() const { return Agility; }
-	size_t getIntellect() const { return Intellect; }
-	size_t getCunning() const { return Cunning; }
-	size_t getWillpower() const { return Willpower; }
-	size_t getPresence() const { return Presence; }
-
-	void changeBrawn(int value) { Brawn += value; }
-	void changeAgility(int value) { Agility += value; }
-	void changeIntellect(int value) { Intellect += value; }
-	void changeCunning(int value) { Cunning += value; }
-	void changeWillpower(int value) { Willpower += value; }
-	void changePresence(int value) { Presence += value; }
-
 };
 
 class Sturdy final : public Archetype
