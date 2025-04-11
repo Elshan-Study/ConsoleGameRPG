a505b8e3289370a5929a5a288a4392cd7198f3e4
Elshan-Study <isaye_ee51@itstep.edu.az>
2025-04-11 10:50:23 +0400

    Specialization' child added

--- Skill.h
+++ Skill.h
@@ -8,7 +8,7 @@ protected:
 public:
 	Skill() : value(0), dices(0) {};
 	explicit Skill(size_t value) : value(value), dices(0) {};
-	Skill(size_t new_value) { dices -= value; value = new_value;  dices += value; }
+	Skill(size_t new_value) { dices -= value; value = new_value;  dices += value; } /*!Need exceptions: if new_value < 0*/
 
 	virtual void changeValue(int mod, Archetype* parameter) = 0;
 	virtual void setDices(Archetype* parameter) = 0;
@@ -16,7 +16,7 @@ public:
 
 	virtual ~Skill() = default;
 
-	void operator=(size_t new_value) { dices -= value; value = new_value;  dices += value;} /*!Need exceptions: if new_value < 0*/
+	//void operator=(size_t new_value) { dices -= value; value = new_value;  dices += value;} /*!Need exceptions: if new_value < 0*/
 	void operator+=(size_t mod) { dices += mod; value += mod;} /*!Need exceptions: if mod < 0*/
 	void operator-=(size_t mod) { dices -= mod; value -= mod; } /*!Need exceptions: if mod < 0 && mod > value */
 };
@@ -25,7 +25,7 @@ class IntSkill final : public Skill
 {
 public:
 	IntSkill() {};
-	explicit IntSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }
+	/*explicit IntSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }*/
 	IntSkill(size_t new_value) { dices -= value; value = new_value;  dices += value; }
 	
 	void setDices(Archetype* parameter) override
@@ -44,7 +44,7 @@ class BrawnSkill final : public Skill
 {
 public:
 	BrawnSkill() {};
-	explicit BrawnSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }
+	/*explicit BrawnSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }*/
 	BrawnSkill(size_t new_value) { dices -= value; value = new_value;  dices += value; }
 
 	void setDices(Archetype* parameter) override
@@ -63,7 +63,7 @@ class CunningSkill final : public Skill
 {
 public:
 	CunningSkill() {};
-	explicit CunningSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }
+	/*explicit CunningSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }*/
 	CunningSkill(size_t new_value) { dices -= value; value = new_value;  dices += value; }
 
 	void setDices(Archetype* parameter) override
@@ -82,7 +82,7 @@ class AgilitySkill final : public Skill
 {
 public:
 	AgilitySkill() {};
-	explicit AgilitySkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }
+	/*explicit AgilitySkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }*/
 	AgilitySkill(size_t new_value) { dices -= value; value = new_value;  dices += value; }
 
 	void setDices(Archetype* parameter) override
@@ -101,7 +101,7 @@ class WillpowerSkill final : public Skill
 {
 public:
 	WillpowerSkill() {};
-	explicit WillpowerSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }
+	/*explicit WillpowerSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }*/
 	WillpowerSkill(size_t new_value) { dices -= value; value = new_value;  dices += value; }
 
 	void setDices(Archetype* parameter) override
@@ -120,7 +120,7 @@ class PresenceSkill final : public Skill
 {
 public:
 	PresenceSkill() {};
-	explicit PresenceSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }
+	/*explicit PresenceSkill(size_t value, Archetype* parameter) : Skill(value) { setDices(parameter); }*/
 	PresenceSkill(size_t new_value) { dices -= value; value = new_value;  dices += value; }
 
 	void setDices(Archetype* parameter) override
--- Specialization.h
+++ Specialization.h
@@ -27,6 +27,65 @@ public:
 	Specialization(const Specialization&) = delete;
 	Specialization& operator=(const Specialization&) = delete;
 	virtual ~Specialization() = default;
-	
+	/*virtual Item getClassStartItem() = 0;*/
 };
 
+class Wizard final : public Specialization
+{
+public:
+	Wizard()
+	{
+		Alchemy = 2;
+		Magic = 2;
+		Discipline = 2;
+		Perception = 2;
+	}
+};
+
+class Knight final : public Specialization
+{
+public:
+	Knight()
+	{
+		Athletics = 2;
+		Resilience = 2;
+		Melee = 2;
+		Coercion = 2;
+	}
+};
+
+class Archer final : public Specialization
+{
+public:
+	Archer()
+	{
+		Coordination = 2;
+		Ranged = 2;
+		Perception = 2;
+		Survival = 2;
+	}
+};
+
+class Thief final : public Specialization
+{
+public:
+	Thief()
+	{
+		Coordination = 2;
+		Melee = 2;
+		Stealth = 2;
+		Skulduggery = 2;
+	}
+};
+
+class Craftsman final : public Specialization
+{
+public:
+	Craftsman()
+	{
+		Cool = 2;
+		Charm = 2;
+		Negotiation = 2;
+		Mechanics = 2;
+	}
+};
