f19525bc5be8a13e9d77c1c6250add4008068634
Elshan Isayev <elshanisayev378@gmail.com>
2025-04-15 12:07:11 +0400

    Revert "errords"
    This reverts commit 1e0ad658019ca8d40767777fa13769c4d17b3505.

--- Quest.h
+++ Quest.h
@@ -1,8 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "QuestStage.h"
-#include "OptionChoice.h"
+#include "Character.h"
 
 class Quest
 {
@@ -12,84 +11,18 @@ public:
 		Victory,
 		Defeat
 	};
-
 protected:
 	std::string title;
 	bool isFinished;
 	Status currentStatus;
-
-	std::shared_ptr<std::shared_ptr<OptionChoice>[]> choices;
-	std::shared_ptr<std::shared_ptr<QuestStage>[]> stages;
-
-	size_t stagesSize;
-	size_t stagesCapacity;
-
-	size_t choicesSize;
-	size_t choicesCapacity;
-
-	void resizeStages() {
-		size_t newCapacity = stagesCapacity * 2;
-		auto newArray = std::shared_ptr<std::shared_ptr<QuestStage>[]>(new std::shared_ptr<QuestStage>[newCapacity]);
-		for (size_t i = 0; i < stagesSize; ++i) {
-			newArray[i] = stages[i];
-		}
-		stages = newArray;
-		stagesCapacity = newCapacity;
-	}
-
-	void resizeChoices() {
-		size_t newCapacity = choicesCapacity * 2;
-		auto newArray = std::shared_ptr<std::shared_ptr<OptionChoice>[]>(new std::shared_ptr<OptionChoice>[newCapacity]);
-		for (size_t i = 0; i < choicesSize; ++i) {
-			newArray[i] = choices[i];
-		}
-		choices = newArray;
-		choicesCapacity = newCapacity;
-	}
-
 public:
-	Quest()
-		: title("Unknown"),
-		isFinished(false),
-		currentStatus(Status::Default),
-		stagesSize(0),
-		stagesCapacity(4),
-		choicesSize(0),
-		choicesCapacity(4)
-	{
-		stages = std::shared_ptr<std::shared_ptr<QuestStage>[]>(new std::shared_ptr<QuestStage>[stagesCapacity]);
-		choices = std::shared_ptr<std::shared_ptr<OptionChoice>[]>(new std::shared_ptr<OptionChoice>[choicesCapacity]);
-	}
-
-	explicit Quest(const std::string& title)
-		: title(title),
-		isFinished(false),
-		currentStatus(Status::Default),
-		stagesSize(0),
-		stagesCapacity(4),
-		choicesSize(0),
-		choicesCapacity(4)
-	{
-		stages = std::shared_ptr<std::shared_ptr<QuestStage>[]>(new std::shared_ptr<QuestStage>[stagesCapacity]);
-		choices = std::shared_ptr<std::shared_ptr<OptionChoice>[]>(new std::shared_ptr<OptionChoice>[choicesCapacity]);
-	}
+	Quest() : title("Unknown"), isFinished(false), currentStatus(Status::Default) {};
+	explicit Quest(const std::string& title) : title(title), isFinished(false), currentStatus(Status::Default) {};
 
 	virtual ~Quest() = 0;
 
+	virtual void startQuest(Character& PC) = 0;
+
 	void Win() { currentStatus = Status::Victory; }
 	void Defeat() { currentStatus = Status::Defeat; }
-
-	void addStage(const std::shared_ptr<QuestStage>& stage) {
-		if (stagesSize >= stagesCapacity) {
-			resizeStages();
-		}
-		stages[stagesSize++] = stage;
-	}
-
-	void addChoice(const std::shared_ptr<OptionChoice>& choice) {
-		if (choicesSize >= choicesCapacity) {
-			resizeChoices();
-		}
-		choices[choicesSize++] = choice;
-	}
 };
--- QuestStage.h
+++ QuestStage.h
@@ -9,7 +9,7 @@ protected:
 	size_t index;
 public:
 	QuestStage() : index(0) {};
-	explicit QuestStage(size_t index) : index(index) {};
+	explicit QuestStage(size_t index) : index(index), option(option) {};
 	virtual ~QuestStage() = default;
     size_t getIndex() const { return index; }
 };
